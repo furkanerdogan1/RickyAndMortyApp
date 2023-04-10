5a055331ccc44c80ddf8d74d1ff13a863aac1537
Furkan Erdoğan <edorfs.fe@gmail.com>
2023-04-10 15:21:24 +0300

    Initial Commit

---
+++ RickyAndMortyApp/RickyAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyAndMortyAppApp.swift
+//  RickyAndMortyApp
+//
+//  Created by Furkan Erdoğan on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct RickyAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
