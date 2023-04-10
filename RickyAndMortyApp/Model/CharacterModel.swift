6a52ceb4ee5a571959650cf689bd105854b5b0a3
Furkan Erdoğan <edorfs.fe@gmail.com>
2023-04-10 16:12:59 +0300

    App Created

---
+++ RickyAndMortyApp/Model/CharacterModel.swift
@@ -0,0 +1,25 @@
+//
+//  CharacterModel.swift
+//  RickyAndMortyApp
+//
+//  Created by Furkan Erdoğan on 10.04.2023.
+//
+
+import SwiftUI
+
+struct Character: Codable {
+    let id: Int
+    let name, status, species, type: String
+    let gender: String
+    let origin, location: Location
+    let image: String 
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
+// MARK: - Location
+struct Location: Codable {
+    let name: String
+    let url: String
+}
---
+++ RickyAndMortyApp/Model/LocationModel.swift
@@ -0,0 +1,56 @@
+//
+//  LocationModel.swift
+//  RickyAndMortyApp
+//
+//  Created by Furkan Erdoğan on 10.04.2023.
+//
+
+import SwiftUI
+
+struct LocationSchema: Codable {
+    let info: Info
+    let results: [Result]
+}
+
+// MARK: - Info
+struct Info: Codable {
+    let count, pages: Int
+    let next: String
+    let prev: JSONNull?
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let id: Int
+    let name, type, dimension: String
+    let residents: [String]
+    let url: String
+    let created: String
+}
+
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
